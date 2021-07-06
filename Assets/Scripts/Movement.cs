da28f7ff292bdfec508082b2eab66e543d1eaaa5
Szukács Marcell <spiglebach@gmail.com>
2021-07-06 20:39:00 +0200

    movement - thrust & rotation

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class Movement : MonoBehaviour {
+    private Rigidbody _rigidbody;
+
+    [SerializeField] private float thrust = 20;
+    [SerializeField] private float rotation = 20;
+    
+    void Start() {
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        ProcessThrust();
+        ProcessRotation();
+    }
+    
+    private void ProcessThrust() {
+        if (Input.GetKey(KeyCode.Space)) {
+            _rigidbody.AddRelativeForce(Vector3.up * (thrust * Time.deltaTime));
+        }
+    }
+    
+    private void ProcessRotation() {
+        if (Input.GetKey(KeyCode.D)) {
+            transform.Rotate(0, 0, -rotation * Time.deltaTime);
+        } else if (Input.GetKey(KeyCode.A)) {
+            transform.Rotate(0, 0, rotation * Time.deltaTime);
+        }
     }
 }
