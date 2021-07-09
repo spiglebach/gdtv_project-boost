252f520b5e91e202b2c8cfc48bd66395f39d7e99
Szukács Marcell <spiglebach@gmail.com>
2021-07-09 22:40:02 +0200

    refactor level loading and collision

--- Assets/Scripts/RocketCollision.cs
+++ Assets/Scripts/RocketCollision.cs
@@ -4,30 +4,49 @@ using UnityEngine.SceneManagement;
 
 public class RocketCollision : MonoBehaviour {
     [SerializeField] private float levelEndDelay = 1f;
+
+    private bool gameOver = false;
     
     private void OnCollisionEnter(Collision other) {
+        if (gameOver) return;
         switch (other.gameObject.tag) {
             case "Friendly":
                 // Do not harm player
                 break;
             case "Finish":
-                StartCoroutine(LoadNextLevel());
+                LevelComplete();
                 break;
             default:
-                StartCoroutine(RestartLevel());
+                ExplodeRocket();
                 break;
         }
     }
 
-    private IEnumerator LoadNextLevel() {
+    private void LevelComplete() {
+        gameOver = true;
         // todo display level complete screen
-        yield return new WaitForSeconds(levelEndDelay);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        // todo flash particle effects
+        Invoke("LoadNextLevel", levelEndDelay);
     }
 
-    private IEnumerator RestartLevel() {
+    private void LoadNextLevel() {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex >= SceneManager.sceneCountInBuildSettings) {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    private void ExplodeRocket() {
+        gameOver = true;
         // todo display game over screen
-        yield return new WaitForSeconds(levelEndDelay);
+        // todo explode rocket
+        // todo add sfx
+        Invoke("RestartLevel", levelEndDelay);
+    }
+
+    private void RestartLevel() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
